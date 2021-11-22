7d18db61776b790950329fefa839b14f932b8ba5
unknown <www.dhanrajshetty1280@gmail.com>
2021-11-22 12:35:50 +0530

    setup app for heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forcast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const public = path.join(__dirname)
 const vpaths = path.join(__dirname,'templates/views')
@@ -78,6 +79,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is running')
 })
--- ind.js
+++ ind.js
@@ -13,7 +13,7 @@ form.addEventListener('submit',(e)=>{
     const location = search.value
     msgone.textContent = 'Loading...'
     msgtwo.textContent = ''
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
         if(data.error)
         {
